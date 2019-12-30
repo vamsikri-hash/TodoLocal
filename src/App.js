9c3f046b96ec24742a9366253137d93639fd7c10
vamsikri-hash <vamsikrishna3415@outlook.in>
2019-12-30 12:07:28 +0530

    added components to main app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Form from "./components/forms/Form";
 import "./App.css";
+import Clear from "./components/forms/Clear";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <h1>Hello</h1>
+        <Form />
+        <Clear />
       </div>
     );
   }
