7d36827175581be0e174cdc8411b86ee5f634459
vamsikri-hash <vamsikrishna3415@outlook.in>
2019-12-30 12:06:57 +0530

    new xhange

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,19 @@
-import React from "react";
-
-const Navbar = () => {
-  return (
-    <div className="Navbar bg-blue">
-      <div className="large text-center p ">Todo - S&S</div>
-    </div>
-  );
-};
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+class Navbar extends Component {
+  static defaultProps = {
+    title: "Todo- S&S"
+  };
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  };
+  render() {
+    return (
+      <div className="Navbar bg-blue">
+        <div className="large text-center p ">{this.props.title}</div>
+      </div>
+    );
+  }
+}
 
 export default Navbar;
