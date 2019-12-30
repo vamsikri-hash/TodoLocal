b128d9a7370d78c6e24a2a8f8d1b612e24251683
vamsikri-hash <vamsikrishna3415@outlook.in>
2019-12-30 12:27:21 +0530

    added items components and clear component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import Navbar from "./components/layout/Navbar";
 import Form from "./components/forms/Form";
 import "./App.css";
 import Clear from "./components/forms/Clear";
+import Showitems from "./components/display/Showitems";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Form />
-        <Clear />
+        <div className="side-side">
+          <div className="cont1">
+            <Form />
+            <Clear />
+          </div>
+          <div className="cont2">
+            <Showitems />
+          </div>
+        </div>
       </div>
     );
   }
