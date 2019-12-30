a527e29ac7e34ac27e4342853649bd5ffd238f80
vamsikri-hash <vamsikrishna3415@outlook.in>
2019-12-30 10:37:46 +0530

    added navbar to main

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Hello I'am Navbar</div>;
+  return (
+    <div className="Navbar bg-blue">
+      <div className="large text-center p ">Todo - S&S</div>
+    </div>
+  );
 };
 
 export default Navbar;
